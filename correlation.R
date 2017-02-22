7914eae12ed9c712207a925f1ccd961601eba18b
Antonio Lategano <lategano.antonio@gmail.com>
2017-02-22 14:07:03 +0100

    - Added query file - Changed scripts to automate saving images

--- correlation.R
+++ correlation.R
@@ -1,10 +1,23 @@
-setwd("~/Dropbox (Università)/Cartella del team Università/Risultati/Generi/Acoustic/")
-data = read.csv("alternative correlazione.tsv", sep="\t", header=TRUE)
-#trova righe dove ci sono zeri e le elimina
-row_sub = apply(data, 1, function(row) all(row !=0 ))
-data = data[row_sub,]
+spearman <- function(dir, name) {
+	setwd(dir)
+	data = read.csv(paste(name, " correlazione.tsv", sep=""), sep="\t", header=TRUE)
 
-#calcolo indice di pearson
-sub = paste("Pearson's correlation = ", cor(data, method="pearson"))
+	#trova righe dove ci sono zeri e le elimina
+	row_sub = apply(data, 1, function(row) all(row !=0 ))
+	data = data[row_sub,]
 
-plot(data$weighted.outdegree, data$indegree, log="xy", xlab="Sent Comments", ylab="In degree", sub=sub[2])
+	#calcolo indice di pearson
+	sub = paste("Spearman's correlation = ", cor(data, method="spearman"))
+
+	plot(data$weighted.outdegree, data$indegree, log="xy", xlab="Sent Comments", ylab="In degree", sub=sub[2])
+	par(mar=c(1,1,1,1))
+	quartz.save(file=paste("COR ", name, " correlazione.png", sep=""), dpi=1024)
+	dev.off()
+}
+
+directories = c("Complessivi/", "Generi/Acoustic/", "Generi/Alternative/", "Generi/Electronic/", "Generi/Hip Hop/", "Generi/Pop/", "Generi/Rock/")
+names = c("generale", "acoustic", "alternative", "electronic", "hh", "pop", "rock")
+
+for(i in 1: length(directories)) {
+	spearman(paste("~/Dropbox (Università)/Cartella del team Università/Risultati/", directories[i], sep=""), names[i])
+}
--- low_power_script.R
+++ low_power_script.R
@@ -1,24 +1,47 @@
-library("poweRlaw")
-setwd("~/Desktop")
+loglogPlot <- function(dir, name, type) {
+	library("poweRlaw")
+	setwd(dir)
 
-#read data from csv that has one column with frequency
-data1 <- read.csv("indegree.tsv")
-#extract data from the column to vector
-data2 <- data1[[1]]
+	#read data from csv that has one column with frequency
+	data1 <- read.csv(paste(name, " ", type, ".tsv", sep=""))
+	#extract data from the column to vector
+	data2 <- data1[[1]]
 
-#create a discrete power law object
-d = displ$new(data2)
-#estimate lower bound
-est = estimate_xmin(d)
-d$setXmin(est)
+	#create a discrete power law object
+	d = displ$new(data2)
+	#estimate lower bound
+	est = estimate_xmin(d)
+	d$setXmin(est)
 
-#show a log-log plot of the data
-#On the x axis there are values of comments
-#On the y axis there are CDF (Cumulative Distribution Function), that represents 
-#the probability that the random variable X takes on a value less than or equal to x
-plot1 = plot(d, xlab="In Degree", ylab="CDF")
-#Add in the fitted distribution
-lines(d, col=2)
+	#show a log-log plot of the data
+	#On the x axis there are values of comments
+	#On the y axis there are CDF (Cumulative Distribution Function), that represents 
+	#the probability that the random variable X takes on a value less than or equal to x
+	types = c("indegree", "outdegree", "received comments", "sent comments")
+	if (identical(type, types[1])) {
+		plot(d, xlab="In Degree", ylab="CDF (Cumulative Distribution Function)")
+	} else if (identical(type, types[2])) {
+		plot(d, xlab="Out Degree", ylab="CDF (Cumulative Distribution Function)")
+	} else if (identical(type, types[3])) {
+		plot(d, xlab="Number of received Comments", ylab="CDF (Cumulative Distribution Function)")
+	} else {
+		plot(d, xlab="Number of sent Comments", ylab="CDF (Cumulative Distribution Function)")
+	}
+	#Add in the fitted distribution
+	lines(d, col=2)
+	
+	#print data
+	par(mar=c(1,1,1,1))
+	quartz.save(file=paste("PL ", name, " ", type, ".png", sep=""), dpi=720)
+	dev.off()
+}
 
-#shows data of the plot
-#head(plot1, 100)
+directories = c("Complessivi/", "Generi/Acoustic/", "Generi/Alternative/", "Generi/Electronic/", "Generi/Hip Hop/", "Generi/Pop/", "Generi/Rock/")
+names = c("generale", "acoustic", "alternative", "electronic", "hh", "pop", "rock")
+types = c("indegree", "outdegree", "received comments", "sent comments")
+
+for(j in 1:length(directories)) {
+	for(i in 1:length(types)) {
+		loglogPlot(paste("~/Dropbox (Università)/Cartella del team Università/Risultati/", directories[j], sep=""), names[j], types[i])
+	}
+}
