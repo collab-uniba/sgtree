b9a5c759aa3bf45e6526b6835a09d181ecc5d7a0
Antonio Lategano <lategano.antonio@gmail.com>
2017-02-26 18:00:57 +0100

    Minor corrections

--- overdub net/src/com/company/Main.java
+++ overdub net/src/com/company/Main.java
@@ -50,6 +50,72 @@ public class Main {
         return resultSet;
     }
 
+    private void buildTrees(ResultSet rs) {
+        Tree temp;
+        int tempTreeId;
+        int tempAuthorId;
+
+        try {
+            rs.next();
+
+            temp = new Tree(rs.getInt(1));
+            temp.addAuthor(rs.getInt(2));
+            trees.add(temp);
+
+            while (rs.next()) {
+                tempTreeId = rs.getInt(1);
+                tempAuthorId = rs.getInt(2);
+
+                for (Iterator<Tree> it = trees.iterator(); it.hasNext(); ) {
+                    Tree t = it.next();
+                    if (tempTreeId == t.id) {
+                        t.addAuthor(tempAuthorId);
+                        break;
+                    }
+                    if (!it.hasNext()) {
+                        temp = new Tree(tempTreeId);
+                        temp.addAuthor(tempAuthorId);
+                        trees.add(temp);
+                        break;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void printTrees() {
+        //stampa i vari id degli alberi e i nodi che lo compongono
+        for (Tree t : trees) {
+            System.out.println(t.toString());
+        }
+    }
+
+    private void saveToFile() {
+        try {
+            File fout = new File("Archi OverdubNet 1.tsv");
+            FileOutputStream fos = new FileOutputStream(fout);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+
+            int size;
+            for (Tree t : trees) {
+                size = t.authors.size();
+                if (size != 1) {
+                    for (int i = 0; i < size; i++) {
+                        for (int j = i + 1; j < size; j++) {
+                            bw.write(t.authors.get(i) + "\t" + t.authors.get(j));
+                            bw.newLine();
+                        }
+                    }
+                }
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void closeConnection() {
 
         try {
@@ -80,71 +146,16 @@ public class Main {
 
     }
 
-    private void printTrees() {
-        //stampa i vari id degli alberi e i nodi che lo compongono
-        for (Tree t : trees) {
-            System.out.println(t.toString());
-        }
-    }
-
     public static void main(String[] args) {
         Main main = new Main();
         ResultSet rs;
-        Tree temp;
 
         main.openConnection();
         rs = main.executeQuery("SELECT tree_id, memberId FROM songs WHERE tree_id IS NOT NULL GROUP BY tree_id, memberId");
 
-        try {
-            rs.next();
-
-            temp = new Tree(rs.getInt(1));
-            temp.addAuthor(rs.getInt(2));
-            trees.add(temp);
-
-            while (rs.next()) {
-                for (Iterator<Tree> it = trees.iterator(); it.hasNext(); ) {
-                    Tree t = it.next();
-                    if (rs.getInt("tree_id") == t.id) {
-                        t.addAuthor(rs.getInt("memberId"));
-                        break;
-                    }
-                    if (!it.hasNext()) {
-                        temp = new Tree(rs.getInt("tree_id"));
-                        temp.addAuthor(rs.getInt("memberId"));
-                        trees.add(temp);
-                        break;
-                    }
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            main.closeConnection();
-        }
-
+        main.buildTrees(rs);
         main.printTrees();
+        main.saveToFile();
 
-        try {
-            File fout = new File("Archi OverdubNet 1.tsv");
-            FileOutputStream fos = new FileOutputStream(fout);
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-
-            int size;
-            for (Tree t : trees) {
-                size = t.authors.size();
-                if (size != 1) {
-                    for (int i = 0; i < size; i++) {
-                        for (int j = i + 1; j < size; j++) {
-                            bw.write(t.authors.get(i) + "\t" + t.authors.get(j));
-                            bw.newLine();
-                        }
-                    }
-                }
-            }
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
