aff0f37afd37a1020bbdde279e59a13d009ecad1
Antonio Lategano <lategano.antonio@gmail.com>
2017-04-09 16:39:48 +0200

    - Lo script per il calcolo della distribuzione del grado (pesato e non) prevede che per l’in-degree si considerino solo gli autori, per l’out-degree si considerino autori e commentatori non autori.

--- New Scripts/Power-Law distribution.R
+++ New Scripts/Power-Law distribution.R
@@ -4,49 +4,55 @@ loglogPlot <- function(dir, filename, genre) {
   setwd(dir)
   data = read.csv(paste("CSV/", filename, sep=""), sep="\t")
   
-  data1 = data[data$indegree!=0,]
+  #seleziona solo gli autori in quanto solo loro possono ricevere commenti
+  data1 = data[data$songs_written != 0, ]
+  data1 <- na.omit(data1)  #per i file CSV dei generi toglie i valori N\A dei commentatori non autori, non fa nulla per il file CSV di GENERALE
+  data1$indegree = data1$indegree + 1 #per evitare che i valori pari a 0 vengano ignorati
   data2 = data1$indegree
   d = displ$new(data2)
   est = estimate_xmin(d)
   d$setXmin(est)
   par(bg="white")
-  plot(d, xlab="In Degree", ylab="CDF (Cumulative Distribution Function)", bg="white")
+  plot(d, xlab="In Degree", ylab="CDF (Cumulative Distribution Function)", bg="white", sub=paste("n =", length(data1$indegree)))
   lines(d, col=2)
   par(mar=c(1,1,1,1))
   quartz.save(file=paste("PL", genre, "indegree.png"), dpi=720)
   dev.off()
   
-  data1 = data[data$outdegree!=0,]
-  data2 = data1$outdegree
-  d = displ$new(data2)
+  data$outdegree = data$outdegree + 1 #per evitare che i valori pari a 0 vengano ignorati
+  data1 = data$outdegree
+  d = displ$new(data1)
   est = estimate_xmin(d)
   d$setXmin(est)
   par(bg="white")
-  plot(d, xlab="Out Degree", ylab="CDF (Cumulative Distribution Function)", bg="white")
+  plot(d, xlab="Out Degree", ylab="CDF (Cumulative Distribution Function)", bg="white", sub=paste("n =", length(data1)))
   lines(d, col=2)
   par(mar=c(1,1,1,1))
   quartz.save(file=paste("PL", genre, "outdegree.png"), dpi=720)
   dev.off()
   
-  data1 = data[data$weighted.indegree!=0,]
+  #seleziona solo gli autori in quanto solo loro possono ricevere commenti
+  data1 = data[data$songs_written != 0, ]
+  data1 <- na.omit(data1)  #per i file CSV dei generi toglie i valori N\A dei commentatori non autori, non fa nulla per il file CSV di GENERALE
+  data1$weighted.indegree = data1$weighted.indegree + 1 #per evitare che i valori pari a 0 vengano ignorati
   data2 = data1$weighted.indegree
   d = displ$new(data2)
   est = estimate_xmin(d)
   d$setXmin(est)
   par(bg="white")
-  plot(d, xlab="Number of Comments received", ylab="CDF (Cumulative Distribution Function)", bg="white")
+  plot(d, xlab="Number of Comments received", ylab="CDF (Cumulative Distribution Function)", bg="white", sub=paste("n =", length(data1$weighted.indegree)))
   lines(d, col=2)
   par(mar=c(1,1,1,1))
   quartz.save(file=paste("PL", genre, "comments received.png"), dpi=720)
   dev.off()
   
-  data1 = data[data$weighted.outdegree!=0,]
-  data2 = data1$weighted.outdegree
-  d = displ$new(data2)
+  data$weighted.outdegree = data$weighted.outdegree + 1 #per evitare che i valori pari a 0 vengano ignorati
+  data1 = data$weighted.outdegree
+  d = displ$new(data1)
   est = estimate_xmin(d)
   d$setXmin(est)
   par(bg="white")
-  plot(d, xlab="Number of Comments sent", ylab="CDF (Cumulative Distribution Function)", bg="white")
+  plot(d, xlab="Number of Comments sent", ylab="CDF (Cumulative Distribution Function)", bg="white", sub=paste("n =", length(data1)))
   lines(d, col=2)
   par(mar=c(1,1,1,1))
   quartz.save(file=paste("PL", genre, "comments sent.png"), dpi=720)
